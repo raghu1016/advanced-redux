cdd36ad30415941f3b60d26d00d8c1bbc5fd1e5a
DHANRAJ GOWDA V <70086247+raghu1016@users.noreply.github.com>
2023-12-13 05:05:28 +0000

    Update 17 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,99 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
+let isInitial = true;
 function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => {
+    return state.ui.cartVisible;
+  });
+  const cart = useSelector((state) => {
+    return state.cart;
+  });
+  const notification = useSelector((state) => {
+    return state.ui.notification;
+  });
+  // console.log(showCart);
+
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "pending",
+  //         title: "Sending...",
+  //         message: "Sending cart data",
+  //       }),
+  //     );
+  //     const response = await fetch(
+  //       "https://react-redux-50f47-default-rtdb.firebaseio.com/cart.json",
+  //       {
+  //         method: "PUT",
+  //         body: JSON.stringify(cart),
+  //       },
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Sending cart data failed.");
+  //     }
+
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "success",
+  //         title: "Success!...",
+  //         message: "Sent cart data successfully!",
+  //       }),
+  //     );
+
+  //     // const responseData = await response.json().catch((err) => {
+  //     // });
+  //   };
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+  //   sendCartData().catch((err) => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: "error",
+  //         title: "Error!...",
+  //         message: "Sending cart data failed!",
+  //       }),
+  //     );
+  //   });
+  // }, [cart, dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,22 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => {
+    console.log(state.cart.totalQuantity);
+    return state.cart.totalQuantity;
+  });
+  console.log(cartQuantity);
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
